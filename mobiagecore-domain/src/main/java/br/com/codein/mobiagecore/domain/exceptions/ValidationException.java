e2f997f6bc19dcb0d77259b2783e71c92f55a55e
Luiz Augusto <luizaugusto@sgsistemas.com.br>
2017-05-29 16:09:07 -0300

    Unificando ValidationException no mobiageCore

--- mobiagecore-domain/src/main/java/br/com/codein/mobiagecore/domain/exceptions/ValidationException.java
+++ mobiagecore-domain/src/main/java/br/com/codein/mobiagecore/domain/exceptions/ValidationException.java
@@ -1,7 +1,12 @@
 package br.com.codein.mobiagecore.domain.exceptions;
 
-/**
- * Created by luizaugusto on 29/05/17.
- */
-public class ValidationException {
+public class ValidationException extends RuntimeException{
+
+    /**
+     *
+     * @param message mensagem que será emitida na exceção
+     */
+    public ValidationException(String message) {
+        super(message);
+    }
 }
