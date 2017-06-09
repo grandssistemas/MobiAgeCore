b64f392849157e34e1fc8d0457d6188a6007ed12
Bruno Gelatti <bgelatti@gmail.com>
2017-06-09 15:22:03 -0300

    criando integração com dashboard

--- mobiagecore-api/src/main/java/br/com/codein/mobiagecore/api/DashboardAPI.java
+++ mobiagecore-api/src/main/java/br/com/codein/mobiagecore/api/DashboardAPI.java
@@ -1,11 +1,15 @@
 package br.com.codein.mobiagecore.api;
 
 import br.com.codein.mobiagecore.domain.model.dashboard.Dashboard;
+import br.com.codein.mobiagecore.gateway.dto.DashboardTypeDTO;
 import io.gumga.application.GumgaService;
 import io.gumga.presentation.GumgaAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by gelatti on 09/06/17.
@@ -17,5 +21,10 @@ public class DashboardAPI extends GumgaAPI<Dashboard, Long> {
     @Autowired
     public DashboardAPI(GumgaService<Dashboard, Long> service) {
         super(service);
+    }
+
+    @RequestMapping(value="/getdashboardtypes", method = RequestMethod.GET)
+    public List<DashboardTypeDTO> getDashboardTypes() {
+        return DashboardTypeDTO.getValues();
     }
 }
