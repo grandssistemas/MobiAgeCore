0bc1b30e815732d438ef23f37da42d5aecfa2187
Luiz Augusto <luizaugusto@sgsistemas.com.br>
2017-07-27 09:57:21 -0300

    Criando metodos para criar unidade financeira

---
+++ mobiagecore-domain/src/main/java/br/com/codein/mobiagecore/domain/exceptions/SeedException.java
@@ -0,0 +1,8 @@
+package br.com.codein.mobiagecore.domain.exceptions;
+
+public class SeedException extends RuntimeException {
+
+    public SeedException(String message) {
+        super(message);
+    }
+}
