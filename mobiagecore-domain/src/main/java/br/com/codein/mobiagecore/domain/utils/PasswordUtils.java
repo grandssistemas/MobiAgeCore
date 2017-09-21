b1e872dd839fd32bd0ee99b214be9273b1150cbd
Rafael Mangolin <rbmangolin@gmail.com>
2017-09-21 18:05:08 -0300

    merge

--- mobiagecore-domain/src/main/java/br/com/codein/mobiagecore/domain/utils/PasswordUtils.java
+++ mobiagecore-domain/src/main/java/br/com/codein/mobiagecore/domain/utils/PasswordUtils.java
@@ -1,14 +1,6 @@
 package br.com.codein.mobiagecore.domain.utils;
 
-import java.security.AlgorithmParameters;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -21,60 +13,51 @@ public class PasswordUtils {
     private static int keySize = 128;
     private static byte[] ivBytes;
     private static SecretKey secretKey;
-    private static String password = "22a48f692056962fa773b31e7d98466f9477e75fbec453cceab0585e95dc65f3";
+    private static String plainText2 = "1234";
 
-    static{
+    private static Cipher cipherEncrypt;
+    private static Cipher cipherDecrypt;
+    private static SecretKeySpec secretSpec;
+
+    static {
         try {
-            salt = getSalt();
+            salt = "29wLO5fUE6BFgehVmwEe";
             byte[] saltBytes = salt.getBytes();
-
+            ivBytes = "FOFMORYRSMOEQODT".getBytes("UTF-8");
             SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, iterations, keySize);
+            PBEKeySpec spec = new PBEKeySpec(plainText2.toCharArray(), saltBytes, iterations, keySize);
             secretKey = skf.generateSecret(spec);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            SecretKeySpec secretSpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretSpec);
-            AlgorithmParameters params = cipher.getParameters();
-            ivBytes = params.getParameterSpec(IvParameterSpec.class).getIV();
-        } catch (Exception e) {
-            e.printStackTrace();
+            secretSpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+            cipherEncrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipherEncrypt.init(Cipher.ENCRYPT_MODE, secretSpec, new IvParameterSpec(ivBytes));
+
+        } catch (Exception ex) {
+
         }
+
+
     }
 
     public static String encrypt(char[] plaintext) throws Exception {
-        SecretKeySpec secretSpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretSpec);
-
-        byte[] encryptedTextBytes = cipher.doFinal(String.valueOf(plaintext).getBytes("UTF-8"));
-
+        byte[] encryptedTextBytes = cipherEncrypt.doFinal(String.valueOf(plaintext).getBytes("UTF-8"));
         return DatatypeConverter.printBase64Binary(encryptedTextBytes);
     }
 
     public static String decrypt(char[] encryptedText) throws Exception {
+        cipherDecrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipherDecrypt.init(Cipher.DECRYPT_MODE, secretSpec, new IvParameterSpec(ivBytes));
         byte[] encryptedTextBytes = DatatypeConverter.parseBase64Binary(new String(encryptedText));
-        SecretKeySpec secretSpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretSpec, new IvParameterSpec(ivBytes));
 
         byte[] decryptedTextBytes = null;
 
         try {
-            decryptedTextBytes = cipher.doFinal(encryptedTextBytes);
+            decryptedTextBytes = cipherDecrypt.doFinal(encryptedTextBytes);
         } catch (IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
-
         assert decryptedTextBytes != null;
         return new String(decryptedTextBytes);
 
-    }
-
-    public static String getSalt() throws Exception {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[20];
-        sr.nextBytes(salt);
-        return new String(salt);
     }
 }
